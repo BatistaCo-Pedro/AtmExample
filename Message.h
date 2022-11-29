62b611f4c12dd00be91c0fd43de3485ae5439965
Pedro <pedrocorreiabatista@gmail.com>
2022-11-29 15:54:54 +0100

    changes compelted

--- Message.h
+++ Message.h
@@ -16,11 +16,11 @@ private:
 	int toAccount;
 	Money amount;
 public:
-	inline const int WITHDRAWAL = 0;
-	const int INITIATE_DEPOSIT = 1;
-	const int COMPLETE_DEPOSIT = 2;
-	const int TRANSFER = 3;
-	const int INQUIRY = 4;
+	static const int WITHDRAWAL = 0;
+	static const int INITIATE_DEPOSIT = 1;
+	static const int COMPLETE_DEPOSIT = 2;
+	static const int TRANSFER = 3;
+	static const int INQUIRY = 4;
 
 	Message(int messageCode, Card cardNumber, int pin, int serialNumber,
 		int fromAccount, int toAccount, Money amount);
