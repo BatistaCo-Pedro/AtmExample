c722986b674d006613caffb287ec02762ca0c958
Pedro <pedrocorreiabatista@gmail.com>
2022-11-29 15:41:03 +0100

    some more

--- Deposit.cpp
+++ Deposit.cpp
@@ -1,9 +1,18 @@
 #include "Deposit.h"
+#include "Message.h"
+#include "Receipt.h"
+#include "Atm.h"
+#include "Session.h"
+#include "Card.h"
 
 Deposit::Deposit(Atm atm, Session session, Card card, int pin) {
 
 }
 
 Message Deposit::getSpecificsCustomer() {
+
+}
+
+Receipt Deposit::completeTransaction() {
 
 }
